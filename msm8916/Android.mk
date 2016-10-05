34101a26f0564f6b639efe6d8d7511c455744db6
Harshit Jain <harshitjain6751@gmail.com>
2016-10-05 16:18:24 +0530

    ADD SUPPORT FOR IMS

--- msm8916/Android.mk
+++ msm8916/Android.mk
@@ -42,6 +42,16 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := ims
+LOCAL_MODULE_OWNER := wingtech
+LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libTimeService
 LOCAL_MODULE_OWNER := qcom
 LOCAL_SRC_FILES_64 := system/vendor/lib64/libTimeService.so
