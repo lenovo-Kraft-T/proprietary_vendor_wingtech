e7390c3ea6561660e3c84fa16e142bbeace30ad5
Silentlys <1561707299@qq.com>
2016-10-06 14:06:17 +0800

    msm8916: Fix compile error

--- msm8916/Android.mk
+++ msm8916/Android.mk
@@ -44,7 +44,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := wingtech
-LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
+LOCAL_SRC_FILES := system/app/ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
